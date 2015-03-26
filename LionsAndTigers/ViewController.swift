814c6ae7fe8c2b0d312278a2caaa688fc8aa9f9c
Johnny Awesome <johnslayer1981@gmail.com>
2015-03-26 15:43:18 -0700

    Added struct and created labels

--- LionsAndTigers/ViewController.swift
+++ LionsAndTigers/ViewController.swift
@@ -10,14 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var breedLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var myTiger = Tiger()
+        myTiger.name = "Tigger"
+        myTiger.breed = "Bengal"
+        myTiger.age = 3
+        myTiger.image = UIImage(named: "BengalTiger.jpg")
+        
+        println("My tiger's name is: \(myTiger.name), its age is \(myTiger.age), its breed is \(myTiger.breed) and its image is \(myTiger.image)")
+        
+        myImageView.image = myTiger.image
+        nameLabel.text = myTiger.name
+        breedLabel.text = myTiger.breed
+        ageLabel.text = "\(myTiger.age)"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func nextBarButtonItemPressed(sender: UIBarButtonItem) {
     }
 
 
---
+++ LionsAndTigers/tiger.swift
@@ -0,0 +1,17 @@
+//
+//  tiger.swift
+//  LionsAndTigers
+//
+//  Created by Matt Grimes on 3/26/15.
+//  Copyright (c) 2015 Daily Dram. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Tiger {
+    var age = 0
+    var name = ""
+    var breed = ""
+    var image = UIImage(named:"")
+}
