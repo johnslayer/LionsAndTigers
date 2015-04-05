a9b5640f78c6c26a74b15d19957126d45be2a678
Johnny Awesome <johnslayer1981@gmail.com>
2015-04-04 19:02:48 -0700

    Created Lion Class

---
+++ LionsAndTigersTests/lion.swift
@@ -0,0 +1,18 @@
+//
+//  lion.swift
+//  LionsAndTigers
+//
+//  Created by Matt Grimes on 4/4/15.
+//  Copyright (c) 2015 Daily Dram. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class Lion {
+    var age = 0
+    var isAlphaMale = false
+    var image = UIImage(named:"")
+    var name = ""
+    var subSpecies = ""
+}
